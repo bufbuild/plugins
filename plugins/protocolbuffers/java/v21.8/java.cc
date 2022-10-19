21a245a0e5555147b992494490f6d1e11257732b
pkwarren <pkwarren@users.noreply.github.com>
2022-10-19 00:06:50 +0000

    detected new plugin versions

---
+++ plugins/protocolbuffers/java/v21.8/java.cc
@@ -0,0 +1,7 @@
+#include <google/protobuf/compiler/java/java_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+
+int main(int argc, char *argv[]) {
+  google::protobuf::compiler::java::JavaGenerator generator;
+  return google::protobuf::compiler::PluginMain(argc, argv, &generator);
+}
