23ec9d48f783ab4019511a8fdcaab70de01abe2d
Julian Figueroa <jfigueroa@buf.build>
2025-03-20 15:22:25 -0500

    changes

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -211,9 +211,6 @@ When building a plugin with support for Generated SDKs, consider the following r
 
 ## CI/CD
 
-Builds use [tj-actions/changed-files](https://github.com/tj-actions/changed-files) to determine which plugin(s) need to be rebuilt.
-See [.github/workflows/pr.yaml](.github/workflows/pr.yml) and [internal/cmd/changed-plugins/main.go](internal/cmd/changed-plugins/main.go) for more details.
-
 We use a combination of a custom command ([internal/cmd/fetcher/main.go](internal/cmd/fetcher/main.go)) and Dependabot to keep dependencies up to date in the project.
 The `fetcher` command will use `source.yaml` files in each plugin to determine if new plugin versions are available.
 Dependabot is used to determine if base Docker images are up-to-date with bug/security fixes.
---
+++ internal/git/git.go
@@ -0,0 +1,32 @@
+package git
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/bufbuild/buf/private/pkg/execext"
+)
+
+// ChangedFilesFrom returns the list of file paths that changed, comparing the current git repo
+// against a base Git ref (commit SHA, tag, branch).
+func ChangedFilesFrom(ctx context.Context, ref string) ([]string, error) {
+	var (
+		stdout = bytes.NewBuffer(nil)
+		stderr = bytes.NewBuffer(nil)
+	)
+	if err := execext.Run(
+		ctx,
+		"git",
+		execext.WithArgs("--no-pager", "diff", "--name-only", ref),
+		execext.WithStdout(stdout),
+		execext.WithStderr(stderr),
+	); err != nil {
+		return nil, fmt.Errorf(
+			"run git diff: %w\nstdout: %s\nstderr: %s",
+			err, stdout.String(), stderr.String(),
+		)
+	}
+	return strings.Split(stdout.String(), "\n"), nil
+}
