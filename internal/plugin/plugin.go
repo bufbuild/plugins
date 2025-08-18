1443c43ea9dc717f743ecd5a52a04cf2dc1ece0a
Philip K. Warren <pwarren@buf.build>
2025-08-18 10:55:23 -0500

    Run modernize

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -387,8 +387,8 @@ func filterPluginPaths(filePaths []string, includeTestdata bool) []string {
 			)
 			// calculate the file path relative to the containing plugins dir
 			var relativeFilepath string
-			if strings.HasPrefix(filePath, pluginsDir) {
-				relativeFilepath = strings.TrimPrefix(filePath, pluginsDir)
+			if after, ok := strings.CutPrefix(filePath, pluginsDir); ok {
+				relativeFilepath = after
 			} else if includeTestdata && strings.HasPrefix(filePath, testdataPluginsDir) {
 				relativeFilepath = strings.TrimPrefix(filePath, testdataPluginsDir)
 			}
