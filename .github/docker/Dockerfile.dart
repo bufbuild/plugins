e6106319f383f465cb72ab00b3cf0d063b1cd1df
Philip K. Warren <pkwarren@users.noreply.github.com>
2026-03-19 10:17:52 -0500

    Update base images to debian 13 (#2346)
    Update distroless and build base images (that support it) to debian 13.
    Ran tests on all combinations and the latest plugins that rely on these
    images continue to build/pass locally.
    
    Most of the distroless debian 12 images are EOL this year. Dart and
    Swift don't provide debian 13 images yet and our Rust builds have been
    using alpine all along so not attempting to upgrade them at this time.

--- .github/docker/Dockerfile.dart
+++ .github/docker/Dockerfile.dart
@@ -1,3 +1,3 @@
-FROM dart:3.11.2-sdk
+FROM dart:3.11.2-sdk@sha256:d2a7e07b3e541587415d1281d4865a809149d723cff5b9a5ed6e8a2126b2b2d9
 
 CMD echo this is a dummy file used to automate dependency upgrades for plugins
--- .github/docker/Dockerfile.swift
+++ .github/docker/Dockerfile.swift
@@ -1,3 +1,3 @@
-FROM swift:6.2.4-bookworm
+FROM swift:6.2.4-bookworm@sha256:6ff0d2567e1a9ce6b2bb1212a8d7ade9dc90e6fb02bed349fc664b981c206858
 
 CMD echo this is a dummy file used to automate dependency upgrades for plugins
