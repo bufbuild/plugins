7e344b836fcc4e912690dd03d664498bc88f2dfc
Julian Figueroa <jfigueroa@buf.build>
2026-04-22 12:08:40 -0500

    Prefer flags for --dir, --base-ref, --include-testdata

--- internal/cmd/changed-plugins/main.go
+++ internal/cmd/changed-plugins/main.go
@@ -7,6 +7,7 @@ import (
 
 	"buf.build/go/app/appcmd"
 	"buf.build/go/app/appext"
+	"github.com/spf13/pflag"
 
 	"github.com/bufbuild/plugins/internal/plugin"
 )
@@ -17,21 +18,36 @@ func main() {
 
 func newRootCommand(name string) *appcmd.Command {
 	builder := appext.NewBuilder(name)
+	f := &flags{}
 	return &appcmd.Command{
-		Use:                 name + " <directory>",
+		Use:                 name,
 		Short:               "Outputs plugins that changed relative to a base git ref.",
-		Args:                appcmd.ExactArgs(1),
-		Run:                 builder.NewRunFunc(run),
+		Args:                appcmd.NoArgs,
+		Run:                 builder.NewRunFunc(func(ctx context.Context, container appext.Container) error { return run(ctx, container, f) }),
+		BindFlags:           f.Bind,
 		BindPersistentFlags: builder.BindRoot,
 	}
 }
 
-func run(ctx context.Context, container appext.Container) error {
-	plugins, err := plugin.FindAll(container.Arg(0))
+type flags struct {
+	dir             string
+	baseRef         string
+	includeTestdata bool
+}
+
+func (f *flags) Bind(flagSet *pflag.FlagSet) {
+	flagSet.StringVar(&f.dir, "dir", ".", "directory path to plugins")
+	flagSet.StringVar(&f.baseRef, "base-ref", "", "base git ref to diff against")
+	flagSet.BoolVar(&f.includeTestdata, "include-testdata", false, "include testdata plugin paths in the diff")
+	_ = appcmd.MarkFlagRequired(flagSet, "base-ref")
+}
+
+func run(ctx context.Context, container appext.Container, f *flags) error {
+	plugins, err := plugin.FindAll(f.dir)
 	if err != nil {
 		return fmt.Errorf("find plugins: %w", err)
 	}
-	includedPlugins, err := plugin.FilterByBaseRefDiff(ctx, plugins)
+	includedPlugins, err := plugin.FilterByBaseRefDiff(ctx, plugins, f.baseRef, f.includeTestdata)
 	if err != nil {
 		return fmt.Errorf("filter plugins by changed files: %w", err)
 	}
